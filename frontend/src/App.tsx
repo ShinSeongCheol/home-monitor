4231594986f8ddea517a8e166066195fb7cc43e0
scshin <scshin@sdinfo.co.kr>
2025-07-08 17:25:34 +0900

    feat '온습도 데이터 화면 갱신'

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,12 +4,31 @@ import { useEffect, useState } from 'react'
 
 function App() {
 
+  const [measurementTime, setMesurementTime] = useState<string | number>('--');
   const [temperature, setTemperature] = useState<string | number>('--');
   const [humidity, setHumidity] = useState<string | number>('--');
   
   useEffect(() => {
-    let response = fetch('http://seongcheol.tplinkdns.com:30004/api/v1/dht11/log/last')
-    console.log(response);
+    const fetchData = () => {
+      fetch('http://localhost:8080/api/v1/dht11/log/latest')
+      .then(response => response.json())
+      .then(data => {
+          const measurementTime = data.measurementTime;
+          const temperature = data.temperature;
+          const humidity = data.humidity;
+  
+          setMesurementTime(measurementTime);
+          setTemperature(temperature);
+          setHumidity(humidity);
+        }
+      )
+    }
+
+    fetchData();
+
+    const interval = setInterval(fetchData, 60000);
+
+    return () => clearInterval(interval);
   }, [])
 
   return (
@@ -17,10 +36,10 @@ function App() {
       <div className="app">
         <h1>🏠 Home Monitoring Dashboard</h1>
 
-        <InfoCard id='temp' value={0} unit='°C' label='Temperature'></InfoCard>
-        <InfoCard id='humidity' value={0} unit='%' label='humidity'></InfoCard>
+        <InfoCard id='temp' value={temperature} unit='°C' label='Temperature'></InfoCard>
+        <InfoCard id='humidity' value={humidity} unit='%' label='humidity'></InfoCard>
 
-        <div className="timestamp" id="timestamp">Last updated: --</div>
+        <div className="timestamp" id="timestamp">Last updated: {measurementTime}</div>
       </div>
     </>
   )
