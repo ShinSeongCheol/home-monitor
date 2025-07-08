5040bba2d46fd6ccc8e59640c7a9f18c98c50baa
scshin <scshin@sdinfo.co.kr>
2025-07-08 17:26:07 +0900

    feat 'cors 설정 추가'

---
+++ backend/src/main/java/com/seongcheol/homemonitor/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.seongcheol.homemonitor.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration config = new CorsConfiguration();
+		
+		config.setAllowedOrigins(List.of("http://localhost:5173"));
+		config.setAllowedMethods(List.of("GET"));
+		config.setAllowedHeaders(List.of("*"));
+		config.setAllowCredentials(true);
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/api/v1/**", config);
+		
+		return source;
+	}
+	
+}
