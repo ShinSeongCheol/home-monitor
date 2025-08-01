95be9f380f02238adf16a4402d17d500da51caa7
shinseongcheol <cher3725@naver.com>
2025-08-02 08:31:18 +0900

    refactor "코드 정리"

--- backend/src/main/java/com/seongcheol/homemonitor/controller/SensorController.java
+++ backend/src/main/java/com/seongcheol/homemonitor/controller/SensorController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.seongcheol.homemonitor.dto.Dht11LogDto;
-import com.seongcheol.homemonitor.repository.Dht11LogRepository;
 import com.seongcheol.homemonitor.service.Dht11LogService;
 
 import lombok.RequiredArgsConstructor;
@@ -19,22 +18,25 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class SensorController {
-	
+
 	private final Dht11LogService dht11LogService;
-	
+
 	@GetMapping("/dht11/logs")
 	public List<Dht11LogDto> getLogs() throws Exception {
 		return dht11LogService.getAllLogs();
 	}
-	
+
 	@GetMapping("/dht11/log/range")
-	public List<Dht11LogDto> getLogsBetween(@RequestParam(value = "start") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime start, @RequestParam(value = "end") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime end) throws Exception {
+	public List<Dht11LogDto> getLogsBetween(
+			@RequestParam(value = "start") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime start,
+			@RequestParam(value = "end") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime end)
+			throws Exception {
 		return dht11LogService.getLogsBetween(start, end);
 	}
-	
+
 	@GetMapping("/dht11/log/latest")
 	public Dht11LogDto getLastLog() throws Exception {
 		return dht11LogService.getLatestLog();
 	}
-	
+
 }
