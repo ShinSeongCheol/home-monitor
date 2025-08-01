540044ab06216448d4c85aec4a50b8a6468fe89d
shinseongcheol <cher3725@naver.com>
2025-08-02 07:05:32 +0900

    feat 'cors'
    - cors 설정 추가

--- backend/src/main/java/com/seongcheol/homemonitor/config/CorsConfig.java
+++ backend/src/main/java/com/seongcheol/homemonitor/config/CorsConfig.java
@@ -2,28 +2,44 @@ package com.seongcheol.homemonitor.config;
 
 import java.util.List;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import lombok.Setter;
+
 @Configuration
+@ConfigurationProperties("cors.allowed")
 public class CorsConfig {
+
+	@Setter
+	private List<String> origins;
 	
+	@Setter
+	private List<String> methods; 
+	
+	@Setter
+	private List<String> headers;
+
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration config = new CorsConfiguration();
-		
-		config.setAllowedOrigins(List.of("http://localhost:5173"));
-		config.setAllowedMethods(List.of("GET"));
-		config.setAllowedHeaders(List.of("*"));
+		config.setAllowedOrigins(origins);
+		config.setAllowedMethods(methods);
+		config.setAllowedHeaders(headers);
 		config.setAllowCredentials(true);
-		
+
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/api/v1/**", config);
-		
+
+		System.out.println(origins);
+		System.out.println(methods);
+		System.out.println(headers);
+
 		return source;
 	}
-	
+
 }
