4bdf6dd3c7c99ce9c0a738457aa8e25ba1ec3ad7
shinseongcheol <cher3725@gmail.com>
2025-08-13 18:00:27 +0900

    ✨ feat 'App.tsx'
    - 단기 예보 위경도 경로 추가
    - 중기 기온 예보 경로 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import styles from './styles/App.module.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './Dashboard';
+import ForecastAdministrativeDistrict from './ForecastAdministrativeDistrict';
+import MiddleForecastAreaDistrict from './MiddleForecastAreaDistrict';
 
 function App() {
 
@@ -9,6 +11,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Dashboard></Dashboard>}></Route>
+          <Route path="/configuration/forecast/administrativeDistrict" element={<ForecastAdministrativeDistrict></ForecastAdministrativeDistrict>}></Route>
+          <Route path="/configuration/forecast/AreaDistrict" element={<MiddleForecastAreaDistrict></MiddleForecastAreaDistrict>}></Route>
+          <Route path="*" element={<div>Page Not Found</div>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
