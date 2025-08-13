1bd1652353c3f7b0c078d28d46b69dcf29e95677
shinseongcheol <cher3725@gmail.com>
2025-08-13 14:41:40 +0900

    ✨ feat 'react-router-dom'
    - react-router 추가
    - App.tsx 코드 분리

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,48 +1,17 @@
 import styles from './styles/App.module.css'
-import InfoCard from './components/InfoCard'
-import { useEffect, useState } from 'react'
-import LineChart from './components/LineChart';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Dashboard from './Dashboard';
 
 function App() {
 
-  const [measurementTime, setMesurementTime] = useState<string | Date>('--');
-  const [temperature, setTemperature] = useState<string | number>('--');
-  const [humidity, setHumidity] = useState<string | number>('--');
-  
-  useEffect(() => {
-    const fetchData = () => {
-      fetch(`${import.meta.env.VITE_API_URL}/api/v1/dht11/log/latest`)
-        .then(response => response.json())
-        .then(data => {
-          const measurementTime = new Date(data.measurementTime);
-          const temperature = data.temperature;
-          const humidity = data.humidity;
-  
-          setMesurementTime(measurementTime);
-          setTemperature(temperature);
-          setHumidity(humidity);
-        }
-      )
-    }
-
-    fetchData();
-
-    const interval = setInterval(fetchData, 60000);
-
-    return () => clearInterval(interval);
-  }, [])
-
   return (
-    <>
-      <div className={styles.app}>
-        <div className={styles.container}>
-          <InfoCard value={temperature} unit='°C' label='🌡️ 온도: '></InfoCard>
-          <InfoCard value={humidity} unit='%' label='💧 습도: '></InfoCard>
-        </div>
-        <LineChart></LineChart>
-        <div className={styles.timestamp} id="timestamp">마지막 업데이트: {measurementTime.toLocaleString()}</div>
-      </div>
-    </>
+    <div className={styles.app}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Dashboard></Dashboard>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
---
+++ frontend/src/Dashboard.tsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+import InfoCard from "./components/InfoCard";
+import LineChart from "./components/LineChart";
+
+import styles from './styles/Dashboard.module.css'
+
+
+const Dashboard = () => {
+    const [measurementTime, setMesurementTime] = useState<string | Date>('--');
+    const [temperature, setTemperature] = useState<string | number>('--');
+    const [humidity, setHumidity] = useState<string | number>('--');
+
+    useEffect(() => {
+        const fetchData = () => {
+            fetch(`${import.meta.env.VITE_API_URL}/api/v1/dht11/log/latest`)
+                .then(response => response.json())
+                .then(data => {
+                    const measurementTime = new Date(data.measurementTime);
+                    const temperature = data.temperature;
+                    const humidity = data.humidity;
+
+                    setMesurementTime(measurementTime);
+                    setTemperature(temperature);
+                    setHumidity(humidity);
+                }
+                )
+        }
+
+        fetchData();
+
+        const interval = setInterval(fetchData, 60000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <>
+            <div className={styles.container}>
+                <InfoCard value={temperature} unit='°C' label='🌡️ 온도: '></InfoCard>
+                <InfoCard value={humidity} unit='%' label='💧 습도: '></InfoCard>
+            </div>
+            <LineChart></LineChart>
+            <div className={styles.timestamp} id="timestamp">마지막 업데이트: {measurementTime.toLocaleString()}</div>
+        </>
+    );
+}
+
+export default Dashboard;
