f71c98516d093b40cb522bf8c62094571dd7b6d7
shinseongcheol <cher3725@naver.com>
2025-08-19 03:47:36 +0900

    ✨ feat 'Member Entity'
    - 멤버 엔티티 추가

---
+++ backend/src/main/java/com/seongcheol/homemonitor/domain/MemberEntity.java
@@ -0,0 +1,34 @@
+package com.seongcheol.homemonitor.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "member")
+public class MemberEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 16)
+    private String name;
+    @Column(length = 128)
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "member_role", joinColumns = @JoinColumn(name = "member_id"))
+    @Column(name = "role", length = 8)
+    private Set<String> role = new HashSet<String>();
+}
