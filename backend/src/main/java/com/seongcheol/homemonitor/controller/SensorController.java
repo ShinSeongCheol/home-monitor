a716bcb6ae18d0282cd4ceefcee6d81fd7b7052e
shinseongcheol <cher3725@gmail.com>
2025-08-22 10:42:11 +0900

    ♻️ refactor 'Sensor 컨트롤러'
    - 생성자 주입에서 Autowired로 관리
    - ResponseEntity를 반환하도록 변경
    - 최신 데이터 조회 예외처리 추가

--- backend/src/main/java/com/seongcheol/homemonitor/controller/SensorController.java
+++ backend/src/main/java/com/seongcheol/homemonitor/controller/SensorController.java
@@ -2,8 +2,11 @@ package com.seongcheol.homemonitor.controller;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.NoSuchElementException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,31 +15,34 @@ import org.springframework.web.bind.annotation.RestController;
 import com.seongcheol.homemonitor.dto.Dht11LogDto;
 import com.seongcheol.homemonitor.service.Dht11LogService;
 
-import lombok.RequiredArgsConstructor;
-
 @RestController
 @RequestMapping("/api/v1")
-@RequiredArgsConstructor
 public class SensorController {
 
-	private final Dht11LogService dht11LogService;
+	@Autowired
+	private Dht11LogService dht11LogService;
 
 	@GetMapping("/dht11/logs")
-	public List<Dht11LogDto> getLogs() throws Exception {
-		return dht11LogService.getAllLogs();
+	public ResponseEntity<List<Dht11LogDto>> getLogs() throws Exception {
+		return ResponseEntity.ok(dht11LogService.getAllLogs());
 	}
 
 	@GetMapping("/dht11/log/range")
-	public List<Dht11LogDto> getLogsBetween(
+	public ResponseEntity<List<Dht11LogDto>> getLogsBetween(
 			@RequestParam(value = "start") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime start,
 			@RequestParam(value = "end") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime end)
 			throws Exception {
-		return dht11LogService.getLogsBetween(start, end);
+				
+		return ResponseEntity.ok(dht11LogService.getLogsBetween(start, end));
 	}
 
 	@GetMapping("/dht11/log/latest")
-	public Dht11LogDto getLastLog() throws Exception {
-		return dht11LogService.getLatestLog();
+	public ResponseEntity<Dht11LogDto> getLastLog() throws Exception {
+		try{
+			return ResponseEntity.ok(dht11LogService.getLatestLog());
+		}catch(NoSuchElementException e) {
+			return ResponseEntity.noContent().build();
+		}
 	}
 
 }
