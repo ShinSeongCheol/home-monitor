cb50b1fecdbff49032e0beca5034c2633c397619
shinseongcheol <cher3725@naver.com>
2025-08-25 01:13:37 +0900

    ♻️ refactor '코드 정리'

--- backend/src/main/java/com/seongcheol/homemonitor/repository/AdministrativeDistrictRepository.java
+++ backend/src/main/java/com/seongcheol/homemonitor/repository/AdministrativeDistrictRepository.java
@@ -1,9 +1,24 @@
 package com.seongcheol.homemonitor.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.seongcheol.homemonitor.domain.AdministrativeDistrictEntity;
 
 public interface AdministrativeDistrictRepository extends JpaRepository<AdministrativeDistrictEntity, Long> {
+
+    @Query("""
+            SELECT 
+                fad
+            FROM
+                AdministrativeDistrictEntity fad
+            WHERE
+                (fad.level2 <> '' AND fad.level2 IS NOT NULL)
+                AND
+                (fad.level3 = '' OR fad.level3 IS NULL)
+            """)
+    List<AdministrativeDistrictEntity> getLevel2List();
     
 }
