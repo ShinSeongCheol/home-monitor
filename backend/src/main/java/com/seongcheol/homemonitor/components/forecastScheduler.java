42099ab82212423c4ae0f7a5c42b0a60f531b58f
shinseongcheol <cher3725@naver.com>
2025-08-26 19:33:08 +0900

    ✨ feat '초단기 실황 조회'
    - prod 환경에서만 작동되도록 변경

--- backend/src/main/java/com/seongcheol/homemonitor/components/forecastScheduler.java
+++ backend/src/main/java/com/seongcheol/homemonitor/components/forecastScheduler.java
@@ -3,12 +3,14 @@ package com.seongcheol.homemonitor.components;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.seongcheol.homemonitor.service.ForecastService;
 
 @Component
+@Profile("prod")
 public class forecastScheduler {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
