0dae014d99808a053cb4c1408038acdfa8ced498
shinseongcheol <cher3725@gmail.com>
2025-08-26 17:50:47 +0900

    💄 feat '라인 차트 컴포넌트'
    - x축 시간 변경
    - data전달 해서 표시되도록 변경

--- frontend/src/Dashboard.tsx
+++ frontend/src/Dashboard.tsx
@@ -4,13 +4,50 @@ import LineChartComponent from './components/LineChartComponent';
 import HumiditySVG from './assets/humidity.svg?react';
 import TemeratureSVG from './assets/device_thermostat.svg?react'
 
+export type Data = {
+    x: Date;
+    y: number;
+};
+
+export type Datasets = {
+    name: string; 
+    data: Data[]; 
+    color: string
+}
+
 const Dashboard = () => {
+    
+    const data1: Data[] = [
+        { x: new Date(), y: 1 },
+        { x: new Date(), y: 3 },
+        { x: new Date(), y: 2 },
+        { x: new Date(), y: 6 },
+        { x: new Date(), y: 5 },
+        { x: new Date(), y: 9 },
+    ];
+    
+    const data2: Data[] = [
+        { x: new Date(), y: 10 },
+        { x: new Date(), y: 30 },
+        { x: new Date(), y: 20 },
+        { x: new Date(), y: 60 },
+        { x: new Date(), y: 50 },
+        { x: new Date(), y: 90 },
+    ];
+    
+    const datasets: Datasets[] = [
+        { name: "A", data: data1, color: "steelblue" },
+        { name: "B", data: data2, color: "tomato" }
+    ];
+
+    console.log(datasets);
+
     return (
         <main className={styles.main}>
             <section className={styles.section}>
                 <WeatherComponent></WeatherComponent>
-                <LineChartComponent title='온도 추이' icon={<TemeratureSVG width={"24px"} height={"24px"} fill='#ffa2a2ff'/>}></LineChartComponent>
-                <LineChartComponent title='습도 추이' icon={<HumiditySVG width={"24px"} height={"24px"} fill='#99ddfdff'/>}></LineChartComponent>
+                <LineChartComponent title='온도 추이' icon={<TemeratureSVG width={"24px"} height={"24px"} fill='#ffa2a2ff' />} datasets={datasets}></LineChartComponent>
+                <LineChartComponent title='습도 추이' icon={<HumiditySVG width={"24px"} height={"24px"} fill='#99ddfdff' />} datasets={datasets}></LineChartComponent>
             </section>
         </main>
     );
--- frontend/src/components/LineChartComponent.tsx
+++ frontend/src/components/LineChartComponent.tsx
@@ -1,14 +1,16 @@
 import { useMemo, useRef } from 'react';
 import styles from '../styles/LineChartComponent.module.css';
 import * as d3 from 'd3';
+import type { Data, Datasets } from '../Dashboard';
 
 
 type LineChartComponentProps = {
     title: string;
-    icon: React.ReactNode
+    icon: React.ReactNode;
+    datasets: Datasets[];
 };
 
-const LineChartComponent = ({title, icon} : LineChartComponentProps) => {
+const LineChartComponent = ({ title, icon, datasets }: LineChartComponentProps) => {
 
     const containerRef = useRef<HTMLDivElement | null>(null);
     const containerWidth = containerRef.current?.offsetWidth ?? 1024;
@@ -19,27 +21,36 @@ const LineChartComponent = ({title, icon} : LineChartComponentProps) => {
     const marginBottom = 20;
     const marginLeft = 40;
 
-    const xDomain = [0,100]
+    const xDomain = [d3.timeDay.floor(new Date()), d3.timeHour.offset(d3.timeDay.ceil(new Date()), -1)]
     const xRange = [marginLeft, containerWidth - marginRight]
 
+    const yDomain = [100, 0]
+    const yRange = [marginBottom, containerHeight - marginTop]
+
+    const xScale = useMemo(() => {
+        return d3.scaleTime().domain(xDomain).range(xRange);
+    }, [containerWidth])
+
+    const yScale = useMemo(() => {
+        return d3.scaleLinear().domain(yDomain).range(yRange);
+    }, [containerHeight])
+
     const xTicks = useMemo(() => {
-        const xScale = d3.scaleLinear()
+        const xScale = d3.scaleTime()
             .domain(xDomain)
             .range(xRange)
 
+        const timeFormatter = d3.timeFormat("%H:%M")
         const width = xRange[1] - xRange[0];
         const pixelsPerTick = 30;
         const numberOfTicksTarget = Math.max(1, Math.floor(width / pixelsPerTick));
 
         return xScale.ticks(numberOfTicksTarget)
             .map(value => ({
-                value,
+                value: timeFormatter(value),
                 xOffset: xScale(value)
             }));
     }, [containerWidth])
-
-    const yDomain = [100, 0]
-    const yRange = [marginBottom, containerHeight - marginTop]
 
     const yTicks = useMemo(() => {
         const yScale = d3.scaleLinear()
@@ -57,27 +68,37 @@ const LineChartComponent = ({title, icon} : LineChartComponentProps) => {
             }));
     }, [containerHeight])
 
-    console.log(yTicks);
+    const lineGenerator = d3.line<Data>()
+        .x(d => xScale(d.x))
+        .y(d => yScale(d.y));
 
     return (
         <div className={styles.container} ref={containerRef}>
             <h2>{icon} {title}</h2>
             <svg width={containerWidth} height={containerHeight}>
-                <path d={["M", -6, yRange[0], "H", 0, "V", yRange[1], "H", -6].join(" ")} fill='none' stroke='currentColor' transform={`translate(${marginLeft}, 0)`}></path>
-                {yTicks.map(({ value, yOffset }) => (
-                    <g key={value} transform={`translate(${marginLeft}, ${yOffset})`}>
-                        <line x2="-6" stroke='currentColor'></line>
-                        <text key={value} style={{fontSize: "10px", textAnchor: "middle", transform: "translateX(-20px)"}}>{value}</text>
-                    </g>
-                ))}
+                <g>
+                    <path d={["M", -6, yRange[0], "H", 0, "V", yRange[1], "H", -6].join(" ")} fill='none' stroke='currentColor' transform={`translate(${marginLeft}, 0)`}></path>
+                    {yTicks.map(({ value, yOffset }) => (
+                        <g key={value} transform={`translate(${marginLeft}, ${yOffset})`}>
+                            <line x2="-6" stroke='currentColor'></line>
+                            <text key={value} style={{ fontSize: "10px", textAnchor: "middle", transform: "translateX(-20px)" }}>{value}</text>
+                        </g>
+                    ))}
+                </g>
 
-                <path d={["M", xRange[0], 6, "v", -6, "H", xRange[1], "v", 6].join(" ")} fill='none' stroke='currentColor' transform={`translate(0, ${containerHeight - marginBottom})`}></path>
-                {xTicks.map(({ value, xOffset }) => (
-                    <g key={value} transform={`translate(${xOffset}, ${containerHeight - marginBottom})`}>
-                        <line y2="6" stroke='currentColor'></line>
-                        <text key={value} style={{fontSize: "10px", textAnchor: "middle", transform: "translateY(20px)"}}>{value}</text>
-                    </g>
-                ))}
+                <g>
+                    <path d={["M", xRange[0], 6, "v", -6, "H", xRange[1], "v", 6].join(" ")} fill='none' stroke='currentColor' transform={`translate(0, ${containerHeight - marginBottom})`}></path>
+                    {xTicks.map(({ value, xOffset }) => (
+                        <g key={value} transform={`translate(${xOffset}, ${containerHeight - marginBottom})`}>
+                            <line y2="6" stroke='currentColor'></line>
+                            <text key={value} style={{ fontSize: "10px", textAnchor: "middle", transform: "translateY(20px)" }}>{value}</text>
+                        </g>
+                    ))}
+                </g>
+
+                {datasets.map((data) => {
+                    return <path key={data.name} d={lineGenerator(data.data) ?? ""} fill='none' stroke={data.color} strokeWidth={'2'}></path>
+                })}
             </svg>
         </div>
     )
