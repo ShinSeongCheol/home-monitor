9b57c98fb67f47029587d41f5966133743430b00
shinseongcheol <cher3725@naver.com>
2025-08-27 03:08:52 +0900

    ✨ feat '카드 컴포넌트'
    - 데이터 조회 추가

--- frontend/src/components/WeatherComponent.tsx
+++ frontend/src/components/WeatherComponent.tsx
@@ -1,8 +1,42 @@
 import styles from '../styles/weatherComponent.module.css';
 import HumiditySVG from '../assets/humidity.svg?react';
 import TemeratureSVG from '../assets/device_thermostat.svg?react'
+import { useEffect, useState } from 'react';
 
 const weatherComponent = () => {
+
+    const [insideTemperature, setInsideTemperature] = useState<string | null>(null);
+    const [insideHumidity, setInsideHumidity] = useState<string | null>(null);
+    const [insideMeasurementTime, setInsideMeasurementTime] = useState<string | null>(null);
+
+    useEffect(() => {
+
+        const fetchData = () => {
+            fetch(`${import.meta.env.VITE_API_URL}/api/v1/dht11/log/latest`)
+                .then(res => {
+                    if (res.ok) {
+                        return res.json();
+                    } else {
+                        throw new Error(`Http Error : ${res.status}`);
+                    }
+                })
+                .then(data => {
+                    const temperature = data.temperature;
+                    const humidity = data.humidity;
+                    const measurementTime:Date = new Date(data.measurementTime);
+
+                    setInsideTemperature(temperature);
+                    setInsideHumidity(humidity);
+                    setInsideMeasurementTime(measurementTime.toLocaleString());
+                })
+        }
+
+        fetchData();
+
+        const interval = setInterval(fetchData, 1000 * 60);
+        return () => clearInterval(interval);
+    }, [])
+
     return (
         <div className={styles.container}>
             <div className={styles.card}>
@@ -11,8 +45,8 @@ const weatherComponent = () => {
                     <span><TemeratureSVG width={"24px"} height={"24px"} fill='#ffa2a2ff'></TemeratureSVG></span>
                 </div>
                 <div className={styles.cardContent}>
-                    <span>36.3°C</span>
-                    <p>2025.08.25</p>
+                    <span>{`${insideTemperature ?? ""}°C`}</span>
+                    <p>{insideMeasurementTime}</p>
                 </div>
             </div>
             <div className={styles.card}>
@@ -21,8 +55,8 @@ const weatherComponent = () => {
                     <span><HumiditySVG width={"24px"} height={"24px"} fill='#99ddfdff'></HumiditySVG></span>
                 </div>
                 <div className={styles.cardContent}>
-                    <span>73.1%</span>
-                    <p>2025.08.25</p>
+                    <span>{`${insideHumidity ?? ""}%`}</span>
+                    <p>{insideMeasurementTime}</p>
                 </div>
             </div>
             <div className={styles.card}>
