577c62df2c28cb5f71ff1bf22fd6ab00cdf49803
shinseongcheol <cher3725@gmail.com>
2025-08-26 16:40:10 +0900

    💄 feat '라인 차트 컴포넌트'
    - 온도, 습도 라인 차트 컴포넌트 추가

--- frontend/src/Dashboard.tsx
+++ frontend/src/Dashboard.tsx
@@ -1,14 +1,16 @@
 import styles from './styles/Dashboard.module.css'
 import WeatherComponent from "./components/WeatherComponent";
 import LineChartComponent from './components/LineChartComponent';
-
+import HumiditySVG from './assets/humidity.svg?react';
+import TemeratureSVG from './assets/device_thermostat.svg?react'
 
 const Dashboard = () => {
     return (
         <main className={styles.main}>
             <section className={styles.section}>
                 <WeatherComponent></WeatherComponent>
-                <LineChartComponent></LineChartComponent>
+                <LineChartComponent title='온도 추이' icon={<TemeratureSVG width={"24px"} height={"24px"} fill='#ffa2a2ff'/>}></LineChartComponent>
+                <LineChartComponent title='습도 추이' icon={<HumiditySVG width={"24px"} height={"24px"} fill='#99ddfdff'/>}></LineChartComponent>
             </section>
         </main>
     );
--- frontend/src/components/LineChartComponent.tsx
+++ frontend/src/components/LineChartComponent.tsx
@@ -1,44 +1,87 @@
-import { useEffect, useRef } from 'react';
+import { useMemo, useRef } from 'react';
 import styles from '../styles/LineChartComponent.module.css';
 import * as d3 from 'd3';
 
-const LineChartComponent = () => {
-    const svgRef = useRef<SVGSVGElement | null>(null);
 
-    useEffect(() => {
-        const svgElement = d3.select(svgRef.current);
+type LineChartComponentProps = {
+    title: string;
+    icon: React.ReactNode
+};
 
-        const width = svgRef.current?.getBoundingClientRect().width ?? 1024;
-        const height = 400;
+const LineChartComponent = ({title, icon} : LineChartComponentProps) => {
 
-        const marginTop = 20;
-        const marginRight = 30;
-        const marginBottom = 20;
-        const marginLeft = 20;
+    const containerRef = useRef<HTMLDivElement | null>(null);
+    const containerWidth = containerRef.current?.offsetWidth ?? 1024;
+    const containerHeight = containerRef.current?.offsetHeight ?? 300;
 
-        svgElement.attr('viewBox', `0 0 ${width} ${height}`);
+    const marginTop = 20;
+    const marginRight = 20;
+    const marginBottom = 20;
+    const marginLeft = 40;
 
+    const xDomain = [0,100]
+    const xRange = [marginLeft, containerWidth - marginRight]
 
-        const xScale = d3.scaleLinear().domain([0, 100]).range([0, width]);
-        const yScale = d3.scaleLinear().domain([0, 100]).range([height, 0]);
+    const xTicks = useMemo(() => {
+        const xScale = d3.scaleLinear()
+            .domain(xDomain)
+            .range(xRange)
 
-        const xAxis = d3.axisBottom(xScale);
-        const yAxis = d3.axisRight(yScale);
+        const width = xRange[1] - xRange[0];
+        const pixelsPerTick = 30;
+        const numberOfTicksTarget = Math.max(1, Math.floor(width / pixelsPerTick));
 
-        svgElement.append("g")
-        .attr("transform", `translate(0, ${height - marginBottom})`)
-        .call(xAxis);
+        return xScale.ticks(numberOfTicksTarget)
+            .map(value => ({
+                value,
+                xOffset: xScale(value)
+            }));
+    }, [containerWidth])
 
-        svgElement.append("g")
-        .attr("transform", `translate(${marginLeft}, 0)`)
-        .call(yAxis);
-    }, [])
+    const yDomain = [100, 0]
+    const yRange = [marginBottom, containerHeight - marginTop]
 
-    return(
-        <div className={styles.container}>
-            <svg ref={svgRef} className={styles.chartSvg}></svg>
+    const yTicks = useMemo(() => {
+        const yScale = d3.scaleLinear()
+            .domain(yDomain)
+            .range(yRange)
+
+        const height = yRange[1] - yRange[0];
+        const pixelsPerTick = 10;
+        const numberOfTicksTarget = Math.max(1, Math.floor(height / pixelsPerTick));
+
+        return yScale.ticks(numberOfTicksTarget)
+            .map(value => ({
+                value,
+                yOffset: yScale(value)
+            }));
+    }, [containerHeight])
+
+    console.log(yTicks);
+
+    return (
+        <div className={styles.container} ref={containerRef}>
+            <h2>{icon} {title}</h2>
+            <svg width={containerWidth} height={containerHeight}>
+                <path d={["M", -6, yRange[0], "H", 0, "V", yRange[1], "H", -6].join(" ")} fill='none' stroke='currentColor' transform={`translate(${marginLeft}, 0)`}></path>
+                {yTicks.map(({ value, yOffset }) => (
+                    <g key={value} transform={`translate(${marginLeft}, ${yOffset})`}>
+                        <line x2="-6" stroke='currentColor'></line>
+                        <text key={value} style={{fontSize: "10px", textAnchor: "middle", transform: "translateX(-20px)"}}>{value}</text>
+                    </g>
+                ))}
+
+                <path d={["M", xRange[0], 6, "v", -6, "H", xRange[1], "v", 6].join(" ")} fill='none' stroke='currentColor' transform={`translate(0, ${containerHeight - marginBottom})`}></path>
+                {xTicks.map(({ value, xOffset }) => (
+                    <g key={value} transform={`translate(${xOffset}, ${containerHeight - marginBottom})`}>
+                        <line y2="6" stroke='currentColor'></line>
+                        <text key={value} style={{fontSize: "10px", textAnchor: "middle", transform: "translateY(20px)"}}>{value}</text>
+                    </g>
+                ))}
+            </svg>
         </div>
     )
+
 }
 
 export default LineChartComponent;
