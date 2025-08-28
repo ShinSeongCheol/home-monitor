df980bdbbe75a4e38f593ff3cc0d5870ae8e4d7b
shinseongcheol <cher3725@gmail.com>
2025-08-28 13:55:56 +0900

    ✨ feat '회원가입 컴포넌트'
    - 회원가입 기능

--- frontend/src/components/SignupFormComponent.tsx
+++ frontend/src/components/SignupFormComponent.tsx
@@ -1,27 +1,62 @@
+import { useState, type FormEventHandler } from 'react';
 import styles from '../styles/SingupFormComponent.module.css'
+import { useNavigate } from 'react-router-dom';
 
-type LoginFormProps = {
-    handleSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
-};
+const SignupFormComponent = () => {
 
-const SignupFormCompoenent: React.FC<LoginFormProps> = ({ handleSubmit }) => {
+    const [nickname, setNickname] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordConfirm, setPasswordConfirm] = useState("");
+
+    const navigate = useNavigate();
+
+    const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+        event.preventDefault();
+
+        if(password !== passwordConfirm) {
+            alert('비밀번호가 일치하지 않습니다.');
+            return;
+        }
+        
+        fetch(`${import.meta.env.VITE_API_URL}/api/v1/member/signup`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: nickname,
+                password,
+            }),
+        })
+        .then(res => {
+            if (res.ok) {
+                alert('회원 가입 성공하였습니다');
+                navigate('/');
+            }else {
+                throw new Error(`HTTP ERROR : ${res.status}`);
+            }
+        }).catch(error => {
+            console.error(error);
+        })
+    }
+
     return (
         <form className={styles.loginForm} onSubmit={handleSubmit}>
             <div className={styles.inputContainer}>
-                <label htmlFor="">아이디</label>
-                <input type="text" name="signup_id" id="signup_id" placeholder='Nickname'/>
+                <label htmlFor="nickname">아이디</label>
+                <input type="text" name="nickname" id="nickname" required placeholder='Nickname' onChange={(event) => setNickname(event.target.value)}/>
             </div>
             <div className={styles.inputContainer}>
-                <label htmlFor="">비밀번호</label>
-                <input type="password" name="signup_password" id="signup_password" placeholder='••••••••'/>
+                <label htmlFor="password">비밀번호</label>
+                <input type="password" name="password" id="password" required placeholder='••••••••' onChange={(event) => setPassword(event.target.value)}/>
             </div>
             <div className={styles.inputContainer}>
-                <label htmlFor="">비밀번호 확인</label>
-                <input type="password" name="signup_password_confirm" id="signup_password_confirm" placeholder='••••••••'/>
+                <label htmlFor="password_confirm">비밀번호 확인</label>
+                <input type="password" name="password_confirm" id="password_confirm" required placeholder='••••••••' onChange={(event) => setPasswordConfirm(event.target.value)}/>
             </div>
             <input type="submit" value="회원가입" />
         </form>
     )
 }
 
-export default SignupFormCompoenent;
+export default SignupFormComponent;
