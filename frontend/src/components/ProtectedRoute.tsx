d166dcaf4509f920df1e0a04fa6bf5d3a413ebb2
shinseongcheol <cher3725@gmail.com>
2025-09-01 11:16:04 +0900

    ♻️ refactor '코드 정리'

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,17 +1,11 @@
-import { Navigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 import type { JSX } from "react";
 
 const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
-    const { isAuthenticated, isLoading } = useAuth();
+    const { isLoading } = useAuth();
 
     if (isLoading) {
         return <div>로딩중...</div>;
-    }
-
-    if (!isAuthenticated) {
-        alert("권한이 없습니다.");
-        return <Navigate to="/" replace />;
     }
 
     return children;
