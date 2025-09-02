c632a5f2c80f81abfe44d1b4e451b4828a0d9beb
shinseongcheol <cher3725@gmail.com>
2025-09-02 11:06:01 +0900

    💄 feat '카드 컴포넌트'
    - 카드 컴포넌트 위치 변경

--- frontend/src/components/WeatherComponent.tsx
+++ frontend/src/components/WeatherComponent.tsx
@@ -51,22 +51,22 @@ const weatherComponent = () => {
             </div>
             <div className={styles.card}>
                 <div className={styles.cardHeader}>
-                    <p>실내 습도</p>
-                    <span><HumiditySVG width={"24px"} height={"24px"} fill='#99ddfdff'></HumiditySVG></span>
-                </div>
-                <div className={styles.cardContent}>
-                    <span>{`${insideHumidity ?? ""}%`}</span>
-                    <p>{insideMeasurementTime}</p>
-                </div>
-            </div>
-            <div className={styles.card}>
-                <div className={styles.cardHeader}>
                     <p>외부 온도</p>
                     <span><TemeratureSVG width={"24px"} height={"24px"} fill='#ffa2a2ff'></TemeratureSVG></span>
                 </div>
                 <div className={styles.cardContent}>
                     <span>36.3°C</span>
                     <p>2025.08.25</p>
+                </div>
+            </div>
+            <div className={styles.card}>
+                <div className={styles.cardHeader}>
+                    <p>실내 습도</p>
+                    <span><HumiditySVG width={"24px"} height={"24px"} fill='#99ddfdff'></HumiditySVG></span>
+                </div>
+                <div className={styles.cardContent}>
+                    <span>{`${insideHumidity ?? ""}%`}</span>
+                    <p>{insideMeasurementTime}</p>
                 </div>
             </div>
             <div className={styles.card}>
