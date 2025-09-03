104a857217c4358a9b88b7bfb461152b981c6c35
shinseongcheol <cher3725@gmail.com>
2025-09-03 17:21:32 +0900

    feat 'ProtectedRoute'
    - 관리자 권한 확인 추가

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,11 +1,28 @@
+import { Navigate, useLocation } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
-import type { JSX } from "react";
+import { type JSX } from "react";
 
 const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
-    const { isLoading } = useAuth();
+    
+    const { user, isLoading } = useAuth();
+    const location = useLocation();
 
     if (isLoading) {
         return <div>로딩중...</div>;
+    }
+    
+    if(!user) {
+        alert("로그인이 필요합니다.");
+        return <Navigate to={'/auth'} replace />; 
+    }
+
+    // 관리자 페이지 접근 관리
+    const adminLocation = ['/configuration/forecast/administrativeDistrict', '/configuration/forecast/AreaDistrict']
+    if (!user.authorities?.includes("ROLE_ADMIN")) {
+        if (adminLocation.includes(location.pathname)) {
+            alert("권한이 없습니다.");
+            return <Navigate to={'/'} replace />
+        }
     }
 
     return children;
