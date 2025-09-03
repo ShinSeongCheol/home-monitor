6504621d23ae6ab5ba11cf0824167b3af318ebd4
shinseongcheol <cher3725@gmail.com>
2025-09-03 14:21:35 +0900

    ✨ feat 'CkEditorComponent'
    - CkEditorComponent 기본 설정

--- frontend/src/components/CkEditorComponent.tsx
+++ frontend/src/components/CkEditorComponent.tsx
@@ -1,6 +1,7 @@
 import { CKEditor } from "@ckeditor/ckeditor5-react";
-import { Autoformat, Bold, ClassicEditor, Code, Essentials, Italic, Paragraph, Strikethrough, Subscript, Superscript, TextTransformation, Underline } from "ckeditor5";
+import { Alignment, Autoformat, BlockQuote, Bold, ClassicEditor, Code, CodeBlock, Essentials, Font, Heading, HorizontalLine, Image, ImageToolbar, ImageCaption, ImageStyle, ImageResize, LinkImage, Indent, IndentBlock, Italic, Link, List, MediaEmbed, Paragraph, Strikethrough, Subscript, Superscript, Table, TableCellProperties, TableColumnResize, TableProperties, TableToolbar, TextTransformation, TodoList, Underline, ImageInsert, Base64UploadAdapter } from "ckeditor5";
 import 'ckeditor5/ckeditor5.css';
+import coreTransitions from "ckeditor5/translations/ko.js";
 
 const CkEditorComponent = () => {
     return(
@@ -8,10 +9,53 @@ const CkEditorComponent = () => {
             editor={ ClassicEditor }
             config={{
                 licenseKey: 'GPL',
-                plugins: [ Autoformat, TextTransformation, Bold, Italic, Underline, Strikethrough, Code, Subscript, Superscript, Essentials, Paragraph ],
-                toolbar: {
-                    items: ['undo', 'redo', '|', 'bold', 'italic', 'underline', 'strikethrough', 'code', 'subscript', 'superscript'],
+                plugins: [ 
+                    Autoformat, TextTransformation, Bold, Italic, Underline, Strikethrough, Code, Subscript, Superscript, Indent,
+                    IndentBlock, BlockQuote, CodeBlock, Font, Essentials, Paragraph, Heading, HorizontalLine, Link, List, TodoList,
+                    MediaEmbed, Alignment, Table, TableToolbar, TableProperties, TableCellProperties, TableColumnResize ,
+                    Image, ImageToolbar, ImageCaption, ImageStyle, ImageResize, LinkImage, ImageInsert, Base64UploadAdapter
+                ],
+                menuBar: {
+                    isVisible: true
                 },
+                toolbar: {
+                    items: [
+                        'heading', 
+                        '|', 
+                        'bold', 'italic', 'underline', 'strikethrough', 'subscript', 'superscript', 'code', 
+                        '|', 
+                        'link', 'insertImage', 'insertTable', 'MediaEmbed', 
+                        '|', 
+                        'Alignment',
+                        '|', 
+                         'horizontalLine', 'bulletedList', 'numberedList', 'TodoList', 'outdent', 'indent', 
+                        '|'
+                    ],
+                },
+                image: {
+                    upload: {
+                        types: ['jpeg', 'png', 'gif', 'bmp', 'weep', 'tiff']
+                    },
+                    toolbar: [
+                        'imageStyle:block',
+                        'imageStyle:side',
+                        '|',
+                        'toggleImageCaption',
+                        'imageTextAlternative',
+                        '|',
+                        'linkImage'
+                    ],
+                    insert: {
+                        integrations: [ 'upload', 'url' ]
+                    }
+                },
+                table: {
+                    contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties'],
+
+                },
+                translations: [
+                    coreTransitions,
+                ],
                 initialData: '<p>Hello World!</p>'
             }}
         />
