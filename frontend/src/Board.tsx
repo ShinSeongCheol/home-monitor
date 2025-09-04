2e7be30cdedfdf645f42a82dde37b76bc4919390
shinseongcheol <cher3725@gmail.com>
2025-09-04 11:35:52 +0900

    💄 feat '게시판'
    - 게시판 목록 페이지 추가

--- frontend/src/Board.tsx
+++ frontend/src/Board.tsx
@@ -1,8 +1,25 @@
+import BoardCardComponent from './components/BoardCardComponent';
+import styles from './styles/Board.module.css';
+
 const Board = () => {
     return(
-        <>
-            <p>게시판 종류 목록</p>
-        </>
+        <main className={styles.main}>
+            <section className={styles.section}>
+                <div className={styles.title}>
+                    <h2>게시판</h2>
+                    <p>다양한 주제의 게시판을 둘러보세요.</p>
+                </div>
+
+                <div className={styles.gridContainer}>
+                    <BoardCardComponent categoryCode='1' categoryName='업데이트 공지'/>
+                    <BoardCardComponent categoryCode='2' categoryName='공지사항'/>
+                    <BoardCardComponent categoryCode='3' categoryName='기술 블로그'/>
+                    <BoardCardComponent categoryCode='4' categoryName='보안 업데이트'/>
+                    <BoardCardComponent categoryCode='5' categoryName='커뮤니티'/>
+                    <BoardCardComponent categoryCode='6' categoryName='설정 가이드'/>
+                </div>
+            </section>
+        </main>
     )
 }
 
---
+++ frontend/src/components/BoardCardComponent.tsx
@@ -0,0 +1,44 @@
+import type { MouseEventHandler } from 'react';
+import styles from '../styles/BoardCardComponent.module.css';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+type BoardCardComponentProps = {
+    categoryCode: string,
+    categoryName: string,
+}
+
+const BoardCardComponent = ({ categoryCode, categoryName }: BoardCardComponentProps) => {
+
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    // 게시판 글 목록 페이지 이동
+    const handleClick: MouseEventHandler<HTMLDivElement> = (e) => {
+        const categoryCode = e.currentTarget.id;
+        navigate(`${location.pathname}/${categoryCode}`);
+    }
+
+    return(
+        <div className={styles.card} id={categoryCode} onClick={handleClick}>
+            <div className={styles.cardHeader}>
+                <h2>{categoryName}</h2>
+                <p>18개 게시글</p>
+            </div>
+
+            <hr />
+
+            <div className={styles.cardBody}>
+                <p>시스템 업데이트와 새로운 기능 소개</p>
+            </div>
+
+            <hr />
+
+            <div className={styles.cardFooter}>
+                <h2>최근게시글</h2>
+                <p>데이터 시각화 차트 개선 사항</p>
+            </div>
+        </div>
+    )
+}
+
+export default BoardCardComponent;
