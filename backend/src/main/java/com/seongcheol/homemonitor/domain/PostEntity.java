b32851a30a0c0ff4a109f5c881875a22670397bd
shinseongcheol <cher3725@gmail.com>
2025-09-09 13:42:09 +0900

    ✨ feat '게시글 엔티티'
    - setter 추가

--- backend/src/main/java/com/seongcheol/homemonitor/domain/PostEntity.java
+++ backend/src/main/java/com/seongcheol/homemonitor/domain/PostEntity.java
@@ -16,11 +16,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 @Entity
 @Table(name = "post")
 @Getter
+@Setter
 @Builder
 @ToString
 @NoArgsConstructor
