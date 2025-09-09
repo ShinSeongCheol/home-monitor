2b4358d834c0ceed32e32fb6c85573753d0f3f49
shinseongcheol <cher3725@gmail.com>
2025-09-09 15:40:58 +0900

    ✨ feat 'ckEditor'
    - Base64에서 simple adapter로 변경

--- frontend/src/components/CkEditorComponent.tsx
+++ frontend/src/components/CkEditorComponent.tsx
@@ -1,15 +1,18 @@
 import { CKEditor } from "@ckeditor/ckeditor5-react";
-import { Alignment, Autoformat, BlockQuote, Bold, ClassicEditor, Code, CodeBlock, Essentials, Font, Heading, HorizontalLine, Image, ImageToolbar, ImageCaption, ImageStyle, ImageResize, LinkImage, Indent, IndentBlock, Italic, Link, List, MediaEmbed, Paragraph, Strikethrough, Subscript, Superscript, Table, TableCellProperties, TableColumnResize, TableProperties, TableToolbar, TextTransformation, TodoList, Underline, ImageInsert, Base64UploadAdapter } from "ckeditor5";
+import { Alignment, Autoformat, BlockQuote, Bold, ClassicEditor, Code, CodeBlock, Essentials, Font, Heading, HorizontalLine, Image, ImageToolbar, ImageCaption, ImageStyle, ImageResize, LinkImage, Indent, IndentBlock, Italic, Link, List, MediaEmbed, Paragraph, Strikethrough, Subscript, Superscript, Table, TableCellProperties, TableColumnResize, TableProperties, TableToolbar, TextTransformation, TodoList, Underline, ImageInsert, SimpleUploadAdapter } from "ckeditor5";
 import 'ckeditor5/ckeditor5.css';
 import coreTransitions from "ckeditor5/translations/ko.js";
 import type { Dispatch, SetStateAction } from "react";
 import styles from '../styles/CkEditorComponent.module.css'
+import { useAuth } from "../contexts/AuthContext";
 
 type CkEditorProps = {
     handleChange: Dispatch<SetStateAction<string>>;
 }
 
 const CkEditorComponent = ({ handleChange }: CkEditorProps) => {
+
+    const {accessToken} = useAuth();
 
     return (
         <div className={styles.editor}>
@@ -21,7 +24,7 @@ const CkEditorComponent = ({ handleChange }: CkEditorProps) => {
                         Autoformat, TextTransformation, Bold, Italic, Underline, Strikethrough, Code, Subscript, Superscript, Indent,
                         IndentBlock, BlockQuote, CodeBlock, Font, Essentials, Paragraph, Heading, HorizontalLine, Link, List, TodoList,
                         MediaEmbed, Alignment, Table, TableToolbar, TableProperties, TableCellProperties, TableColumnResize,
-                        Image, ImageToolbar, ImageCaption, ImageStyle, ImageResize, LinkImage, ImageInsert, Base64UploadAdapter
+                        Image, ImageToolbar, ImageCaption, ImageStyle, ImageResize, LinkImage, ImageInsert, SimpleUploadAdapter
                     ],
                     menuBar: {
                         isVisible: true
@@ -64,6 +67,12 @@ const CkEditorComponent = ({ handleChange }: CkEditorProps) => {
                     translations: [
                         coreTransitions,
                     ],
+                    simpleUpload: {
+                        uploadUrl: `${import.meta.env.VITE_API_URL}/api/v1/boards/image`,
+                        headers: {
+                            'Authorization': `Bearer ${accessToken}`
+                        }
+                    },
                     mediaEmbed: {
                         previewsInData: true
                     }
