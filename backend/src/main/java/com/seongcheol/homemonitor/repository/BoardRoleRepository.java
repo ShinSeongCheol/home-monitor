41a21c912d79358def12aaea88a980fa73173158
shinseongcheol <cher3725@gmail.com>
2025-09-11 11:31:19 +0900

    ✨ feat '게시판 권환 레포지토리'
    - 게시판 권한 관리 레포지토리 추가

---
+++ backend/src/main/java/com/seongcheol/homemonitor/repository/BoardRoleRepository.java
@@ -0,0 +1,9 @@
+package com.seongcheol.homemonitor.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.seongcheol.homemonitor.domain.BoardRoleEntity;
+
+public interface BoardRoleRepository extends JpaRepository<BoardRoleEntity, Long> {
+    
+}
