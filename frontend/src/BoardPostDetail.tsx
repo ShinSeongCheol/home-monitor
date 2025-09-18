cdb1372638e0db5de953408d3a1bf52ad1656a82
shinseongcheol <cher3725@gmail.com>
2025-09-18 16:20:27 +0900

    ✨ feat '게시글 반응'
    - 게시글 반응 기능 추가

--- frontend/src/BoardPostDetail.tsx
+++ frontend/src/BoardPostDetail.tsx
@@ -6,6 +6,7 @@ import 'ckeditor5/ckeditor5.css';
 import { useAuth } from './contexts/AuthContext';
 import Board from './Board';
 import Comment from './components/CommentComponent'
+import FavoriteSVG from './assets/icon/favorite.svg?react';
 
 export type PostComment = {
     id: number;
@@ -19,6 +20,17 @@ export type PostComment = {
     children_comment: PostComment[]
 }
 
+type Reaction = {
+    member: {
+        email: string;
+        nickname: string;
+    },
+    reactionCode: {
+        code: string;
+        name: string;
+    }
+}
+
 const BoardPostDetail = () => {
 
     const {user, accessToken} = useAuth();
@@ -28,6 +40,7 @@ const BoardPostDetail = () => {
 
     const [memberEmail, setMemberEmail] = useState("");
     const [board, setBoard] = useState<Board>();
+    const [reactions, setReactions] = useState<Reaction[]>([]);
 
     const {categoryCode, postId} = useParams();
     const navigate = useNavigate();
@@ -60,12 +73,26 @@ const BoardPostDetail = () => {
                 ADD_TAGS: ["iframe"],
                 ADD_ATTR: ["src", "width", "height", "frameborder", "allow", "allowfullscreen"],
             });
-            
+
             setTitle(DOMPurify.sanitize(data.title));
             setContent(santiizedContent);
 
             setMemberEmail(data.member.email);
             setBoard(data.board);
+        })
+        .catch(err => console.error(err));
+
+        fetch((`${import.meta.env.VITE_API_URL}/api/v1/boards/${categoryCode}/${postId}/reactions`), {
+            headers: {
+                'Content-type': 'application/json'
+            }
+        })
+        .then(res => {
+            if(!res.ok) throw new Error(`Http Error ${res.status}`);
+            return res.json();
+        })
+        .then(res => {
+            setReactions(res);
         })
         .catch(err => console.error(err));
     }, [])
@@ -105,6 +132,55 @@ const BoardPostDetail = () => {
         })
     }
 
+    const handleReact = () => {
+        if(!user?.email) {
+            alert('로그인 후 이용 가능합니다.')
+            return;
+        }
+
+        let isReactionExist = reactions.some((value) => value.member.email === user.email);
+
+        // 반응 삭제
+        if (isReactionExist) {
+            fetch(`${import.meta.env.VITE_API_URL}/api/v1/boards/${categoryCode}/${postId}/reactions`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${accessToken}`
+                },
+                body: JSON.stringify({
+                    code: "HEART"
+                })
+            })
+            .then(res => {
+                if (!res.ok) throw new Error(`Http Error ${res.status}`);
+                setReactions(reactions.filter((value) => value.member.email !== user.email));
+            })
+            .catch(err => console.error(err));
+
+        //반응 추가
+        }else {
+            fetch(`${import.meta.env.VITE_API_URL}/api/v1/boards/${categoryCode}/${postId}/reactions`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${accessToken}`
+                },
+                body: JSON.stringify({
+                    code: "HEART"
+                })
+            })
+            .then(res => {
+                if (!res.ok) throw new Error(`Http Error ${res.status}`);
+                return res.json();
+            })
+            .then(res => {
+                setReactions((prev) => [...prev, res]);
+            })
+            .catch(err => console.error(err));
+        }
+    }
+
     return(
         <main className={styles.main}>
             <section className={styles.section}>
@@ -112,6 +188,9 @@ const BoardPostDetail = () => {
                 <hr />
                 <div className={styles.container}>
                     <div className="ck-content" dangerouslySetInnerHTML={{__html: content}}></div>
+                    <div className={styles.react}>
+                        <FavoriteSVG width={"24px"} height={"24px"} fill={reactions.some(value => value.member.email === user?.email) ? '#f38383ff' : 'black'} onClick={handleReact}/> {reactions.length}
+                    </div>
                 </div>
 
                 <div className={styles.buttonContainer}>
