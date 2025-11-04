ddc3bd3dd70f0993fa167ab0ae5275db6dbd2f73
shinseongcheol <cher3725@naver.com>
2025-11-04 14:00:44 +0900

    :wrench: feat 'kakaoURL'
    - kakaoURL 추가

--- frontend/src/shared/index.ts
+++ frontend/src/shared/index.ts
@@ -1,4 +1,5 @@
-export { backendUrl } from "./config/backendUrl";
-export { sanitize } from './config/domPurify';
+export {backendUrl} from "./config/backendUrl";
+export {kakaoUrl} from './config/kakaoUrl.ts';
+export {sanitize} from './config/domPurify';
 export {AuthProvider, useAuth} from './context/AuthProvider.tsx'
 export type {Auth} from './context/AuthProvider.tsx'
