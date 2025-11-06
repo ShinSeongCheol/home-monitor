c76b50236f5ced2900789edc75353a29265c77fc
shinseongcheol <cher3725@gmail.com>
2025-11-06 15:56:14 +0900

    :recycle: refactor '로그인, 회원가입'
    - 인풋 버튼 높이 조정

--- frontend/src/features/auth/ui/LoginForm.tsx
+++ frontend/src/features/auth/ui/LoginForm.tsx
@@ -9,11 +9,11 @@ export const LoginForm = () => {
         <form className={'mt-4 flex flex-col gap-2'} onSubmit={handleSubmit}>
             <div className={'flex flex-col gap-3'}>
                 <label className={'text-xs'} htmlFor="email">이메일</label>
-                <input className={'h-4 p-2 border-none rounded-sm bg-gray-300'} type="email" name="email" id="email" required placeholder='email@example.com' onChange={handleChangeEmail}/>
+                <input className={'h-8 p-2 border-none rounded-sm bg-gray-300'} type="email" name="email" id="email" required placeholder='email@example.com' onChange={handleChangeEmail}/>
             </div>
             <div className={'flex flex-col gap-3'}>
                 <label className={'text-xs'} htmlFor="password">비밀번호</label>
-                <input className={'h-4 p-2 border-none rounded-sm bg-gray-300'} type="password" name="password" id="password" required placeholder='••••••••' onChange={handleChangePassword}/>
+                <input className={'h-8 p-2 border-none rounded-sm bg-gray-300'} type="password" name="password" id="password" required placeholder='••••••••' onChange={handleChangePassword}/>
             </div>
             <input className={'mt-2 h-12 p-2 border-none text-white bg-black rounded-xl cursor-pointer'} type="submit" value="로그인" />
             <div className={'flex justify-center bg-[#FEE500] rounded-xl cursor-pointer'} onClick={handleLoginKakao}>
--- frontend/src/features/auth/ui/SignupForm.tsx
+++ frontend/src/features/auth/ui/SignupForm.tsx
@@ -8,19 +8,19 @@ export const SignupForm = () => {
         <form className={'mt-4 flex flex-col gap-2'} onSubmit={handleSubmit}>
             <div className={'flex flex-col gap-3'}>
                 <label className={'text-xs'} htmlFor="email">이메일</label>
-                <input className={'h-4 p-2 border-none rounded-sm bg-gray-300'} type="email" name="email" id="email" required placeholder='email@example.com' onChange={handleChangeEmail}/>
+                <input className={'h-8 p-2 border-none rounded-sm bg-gray-300'} type="email" name="email" id="email" required placeholder='email@example.com' onChange={handleChangeEmail}/>
             </div>
             <div className={'flex flex-col gap-3'}>
                 <label className={'text-xs'} htmlFor="nickname">이름</label>
-                <input className={'h-4 p-2 border-none rounded-sm bg-gray-300'} type="text" name="nickname" id="nickname" required placeholder='Nickname' onChange={handleChangeNickname}/>
+                <input className={'h-8 p-2 border-none rounded-sm bg-gray-300'} type="text" name="nickname" id="nickname" required placeholder='Nickname' onChange={handleChangeNickname}/>
             </div>
             <div className={'flex flex-col gap-3'}>
                 <label className={'text-xs'} htmlFor="password">비밀번호</label>
-                <input className={'h-4 p-2 border-none rounded-sm bg-gray-300'} type="password" name="password" id="password" required placeholder='••••••••' onChange={handleChangePassword}/>
+                <input className={'h-8 p-2 border-none rounded-sm bg-gray-300'} type="password" name="password" id="password" required placeholder='••••••••' onChange={handleChangePassword}/>
             </div>
             <div className={'flex flex-col gap-3'}>
                 <label className={'text-xs'} htmlFor="password_confirm">비밀번호 확인</label>
-                <input className={'h-4 p-2 border-none rounded-sm bg-gray-300'} type="password" name="password_confirm" id="password_confirm" required placeholder='••••••••' onChange={handleChangePasswordConfirm}/>
+                <input className={'h-8 p-2 border-none rounded-sm bg-gray-300'} type="password" name="password_confirm" id="password_confirm" required placeholder='••••••••' onChange={handleChangePasswordConfirm}/>
             </div>
             <input className={'mt-2 h-12 p-2 border-none text-white bg-black rounded-xl cursor-pointer'} type="submit" value="회원가입" />
         </form>
