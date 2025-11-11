2ca4e12a4ea39b28b507faa771e98f3073fb0ba3
shinseongcheol <cher3725@gmail.com>
2025-11-11 10:51:32 +0900

    :lipstick: refactor 'FSD 아키텍처화'
    - 백오피스 위젯 생성

--- frontend/src/widgets/backoffice/index.tsx
+++ frontend/src/widgets/backoffice/index.tsx
@@ -1,1 +1,14 @@
 export {BackOfficeBoardWidget} from './ui/BackOfficeBoardWidget.tsx'
+export {BackOfficeBoardRoleWidget} from './ui/BackOfficeBoardRoleWidget.tsx'
+export {BackOfficeBoardRoleCodeWidget} from './ui/BackOfficeBoardRoleCodeWidget.tsx'
+
+export {BackOfficePostWidget} from './ui/BackOfficePostWidget.tsx'
+export {BackOfficeCommentWidget} from './ui/BackOfficeCommentWidget.tsx'
+export {BackOfficeReactionWidget} from './ui/BackOfficeReactionWidget.tsx'
+export {BackOfficeReactionCodeWidget} from './ui/BackOfficeReactionCodeWidget.tsx'
+
+export {BackOfficeUserWidget} from './ui/BackOfficeUserWidget.tsx'
+export {BackOfficeUserRoleWidget} from './ui/BackOfficeUserRoleWidget.tsx'
+export {BackOfficeUserRoleCodeWidget} from './ui/BackOfficeUserRoleCodeWidget.tsx'
+
+export {ForecastAdministrativeDistrictWidget} from './ui/ForecastAdministrativeDistrictWidget.tsx'
---
+++ frontend/src/widgets/backoffice/ui/BackOfficeBoardRoleCodeWidget.tsx
@@ -0,0 +1,11 @@
+import {Nav} from "../../../features/backoffice";
+
+export const BackOfficeBoardRoleCodeWidget = () => {
+    return (
+        <section className={`h-full`}>
+            <div className={'h-full p-4 flex flex-col gap-4'}>
+                <Nav primaryMenu={'관리자'} secondaryMenu={'게시판 관리'} tertiaryMenu={'게시판 권한 코드'}/>
+            </div>
+        </section>
+    )
+}
