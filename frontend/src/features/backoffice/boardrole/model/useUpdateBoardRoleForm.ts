f2f3ca7ab019b6a3688b5db7f738e5ddc55c0079
shinseongcheol <cher3725@gmail.com>
2025-11-11 16:05:39 +0900

    :recycle: refactor 'FSD 아키텍처화'
    - 게시물 관리 게시판 권한 수정 기능 추가

--- frontend/src/features/backoffice/boardrole/model/useUpdateBoardRoleForm.ts
+++ frontend/src/features/backoffice/boardrole/model/useUpdateBoardRoleForm.ts
@@ -7,9 +7,9 @@ import type {
 import {getBackOfficeBoards} from "../../api/getBackOfficeBoards.ts";
 import {getBackOfficeBoardRoleCodes} from "../../api/getBackOfficeBoardRoleCodes.ts";
 import {getBackOfficeMemberRoleCodes} from "../../api/getBackOfficeMemberRoleCodes.ts";
-import {postBoardRole} from "../api/postBoardRole.ts";
 import {useAuth} from "../../../../shared";
 import type {AgGridReact} from "ag-grid-react";
+import {updateBoardRole} from "../api/updateBoardRole.ts";
 
 export const useUpdateBoardRoleForm = (agGridReact: AgGridReact | null) => {
 
@@ -23,62 +23,68 @@ export const useUpdateBoardRoleForm = (agGridReact: AgGridReact | null) => {
 
     const {auth} = useAuth();
 
-    const handleChangeBoardId = (e:ChangeEvent<HTMLSelectElement>) => {
+    const selectedRow: BackOfficeBoardRole = agGridReact?.api.getSelectedRows()[0];
+
+    const handleChangeBoardId = (e: ChangeEvent<HTMLSelectElement>) => {
         setSelectedBoardId(Number(e.target.value));
     }
 
-    const handleChangeBoardRoleCodeId = (e:ChangeEvent<HTMLSelectElement>) => {
+    const handleChangeBoardRoleCodeId = (e: ChangeEvent<HTMLSelectElement>) => {
         setSelectedBoardRoleCodeId(Number(e.target.value));
     }
 
-    const handleChangeMemberRoleCodeId = (e:ChangeEvent<HTMLSelectElement>) => {
+    const handleChangeMemberRoleCodeId = (e: ChangeEvent<HTMLSelectElement>) => {
         setSelectedMemberRoleCodeId(Number(e.target.value));
     }
 
     const handleClickSubmit = async (e: FormEvent<HTMLFormElement>, fetchBoardRoles: () => Promise<void>, handleClickCancel: () => void) => {
         e.preventDefault();
 
+        const selectedRow = agGridReact?.api.getSelectedRows()[0]
+        if (!selectedRow) return;
+
         try {
-            await postBoardRole({boardId: selectedBoardId, boardRoleCodeId: selectedBoardRoleCodeId, memberRoleCodeId: selectedMemberRoleCodeId}, auth?.accessToken);
+            await updateBoardRole(selectedRow.id, {
+                boardId: selectedBoardId,
+                boardRoleCodeId: selectedBoardRoleCodeId,
+                memberRoleCodeId: selectedMemberRoleCodeId
+            }, auth?.accessToken);
             await fetchBoardRoles();
             handleClickCancel();
-        }catch (err) {
+        } catch (err) {
             console.error(err);
         }
     }
 
     const fetchBoards = async () => {
         try {
-            const data:BackOfficeBoard[] = await getBackOfficeBoards();
-            const selectedRow: BackOfficeBoardRole = agGridReact?.api.getSelectedRows()[0];
+            const data: BackOfficeBoard[] = await getBackOfficeBoards();
 
             setBoards(data);
             setSelectedBoardId(selectedRow.board.id);
-        }catch (err) {
+        } catch (err) {
             console.error(err);
         }
     };
 
     const fetchBoardRoleCodes = async () => {
         try {
-            const data:BackOfficeBoardRoleCode[] = await getBackOfficeBoardRoleCodes()
-            const selectedRow: BackOfficeBoardRole = agGridReact?.api.getSelectedRows()[0];
+            const data: BackOfficeBoardRoleCode[] = await getBackOfficeBoardRoleCodes()
 
             setBoardRoleCodes(data)
             setSelectedBoardRoleCodeId(selectedRow.boardRoleCode.id);
-        }catch (err) {
+        } catch (err) {
             console.error(err);
         }
     };
 
     const fetchMemberRoleCodes = async () => {
         try {
-            const data:BackOfficeMemberRoleCode[] = await getBackOfficeMemberRoleCodes();
-            const selectedRow: BackOfficeBoardRole = agGridReact?.api.getSelectedRows()[0];
+            const data: BackOfficeMemberRoleCode[] = await getBackOfficeMemberRoleCodes();
 
             setMemberRoleCodes(data);
             setSelectedMemberRoleCodeId(selectedRow.memberRoleCode.id);
-        }catch (err) {
+        } catch (err) {
             console.error(err);
         }
     };
