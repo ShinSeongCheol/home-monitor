609df0b09bf5b840cebdf42dc6db8a181d6e8c0f
shinseongcheol <cher3725@gmail.com>
2025-11-13 15:32:32 +0900

    :recycle: refactor '게시물 상세보기'
    - 버튼 변경

--- frontend/src/widgets/post/ui/PostDetailWidget.tsx
+++ frontend/src/widgets/post/ui/PostDetailWidget.tsx
@@ -1,12 +1,12 @@
 import {PostDetail} from "../../../entities/post";
 import {PostReaction} from "../../../features/reaction";
-import {CancleButton} from "../../../components/ButtonComponent.tsx";
 import {UpdatePostButton} from "../../../features/post/update";
 import {DeletePostButton} from "../../../features/post/delete";
 import {usePostDetail} from "../../../entities/post";
 import {useNavigate} from "react-router-dom";
 import {useBoard} from "../../../entities/board";
 import {usePostAccess} from "../../../features/post/access";
+import {CancelButton} from "../../../shared/ui";
 
 export const PostDetailWidget = () => {
 
@@ -30,7 +30,7 @@ export const PostDetailWidget = () => {
             }
 
             <div className={'flex justify-end mt-2 gap-2'}>
-                <CancleButton svg={null} type='button' value='목록' onClick={() => navigate(-1)}/>
+                <CancelButton svg={null} type='button' value='목록' onClick={() => navigate(-1)}/>
                 {canModify && <UpdatePostButton/>}
                 {canDelete && <DeletePostButton/>}
             </div>
